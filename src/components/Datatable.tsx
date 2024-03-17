4d189c01a60dd808e219e6f6837e51837f9da718
Zach Coscarart <zachcoscarart@gmail.com>
2024-03-17 03:50:01 -0700

    ugh

--- src/components/Datatable.tsx
+++ src/components/Datatable.tsx
@@ -3,7 +3,7 @@ import Modal from "./Modal";
 import { server_calls } from '../api/server';
 import { useGetData } from '../custom-hooks/FetchData';
 import { Grid, Paper, Typography, Checkbox, Button } from '@mui/material';
-import AuthChecker from '../auth/AuthChecker';
+
 
 const DataTable = () => {
     const { projectData, getData } = useGetData();
@@ -51,7 +51,7 @@ const DataTable = () => {
     }
 
     return (
-        <AuthChecker>
+        <>
             <Modal 
                 id={selectionModel}
                 open={open}
@@ -115,7 +115,7 @@ const DataTable = () => {
                     ))}
                 </Grid>
             </div>
-        </AuthChecker>
+        </>
     )
 }
 
