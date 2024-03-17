fc2b7fa25597f6e7aeb130bba471e4d23af626fd
Zach Coscarart <zachcoscarart@gmail.com>
2024-03-17 03:02:45 -0700

    4 hours of debug later...

--- src/components/Datatable.tsx
+++ src/components/Datatable.tsx
@@ -2,9 +2,7 @@ import { useState } from 'react';
 import Modal from "./Modal";
 import { server_calls } from '../api/server';
 import { useGetData } from '../custom-hooks/FetchData';
-import { Grid, Paper, Typography, Checkbox, Button } from '@mui/material'; // Import Checkbox from Material-UI
-// import AuthChecker from '../auth/AuthChecker.tsx';
-
+import { Grid, Paper, Typography, Checkbox, Button } from '@mui/material';
 
 const DataTable = () => {
     const { projectData, getData } = useGetData();
@@ -45,12 +43,6 @@ const DataTable = () => {
         setSelectionModel(newSelectionModel);
     }
 
-    const checkImageExists = (url: string) => {
-        const img = new Image();
-        img.src = url;
-        return img.complete || (img.height !== 0);
-    };
-
     return (
         <>
             <Modal 
@@ -58,7 +50,6 @@ const DataTable = () => {
                 open={open}
                 onClose={handleClose}
             />
-            {/* <AuthChecker> */}
             <div className="flex flex-row justify-center">
                 <div>
                     <button
@@ -71,7 +62,6 @@ const DataTable = () => {
                 <button onClick={handleOpen} className="p-8 text-3xl bg-yellow-400 text-white rounded m-5  hover:bg-slate-800 hover:text-white" >Update Project</button>
                 <button onClick={deleteData} className="p-8 text-3xl bg-red-600 text-white rounded m-5 hover:bg-slate-800 hover:text-white" >Delete Project</button>
             </div>
-            {/* </AuthChecker> */}
             <div className="container mx-auto ">
                 <h2 className="p-8 bg-black text-white my-2 rounded text-6xl text-center">Current Projects</h2>
                 <Grid container spacing={3}>
@@ -82,35 +72,31 @@ const DataTable = () => {
                                     checked={selectionModel.includes(project.id)}
                                     onChange={() => handleSelectionChange(project.id)}
                                 />
-                                {project.programming_languages && checkImageExists(`../assets/images/${project.programming_languages}.jpg`) ? (
-                                    <img
-                                        src={`../assets/images/${project.programming_languages}.jpg`}
-                                        className="mx-auto mb-3"
-                                        style={{ maxWidth: '40%', height: 'auto' }}
-                                        alt={project.programming_languages}
-                                    />
-                                ) : (
-                                    <img
-                                        src="../assets/images/default.jpg"
-                                        className="mx-auto mb-3"
-                                        style={{ maxWidth: '40%', height: 'auto' }}
-                                        alt="Default"
-                                    />
-                                )}
+                                <div
+                                    className="mx-auto mb-3"
+                                    style={{ 
+                                        maxWidth: '40%', 
+                                        height: 'auto',
+                                        backgroundImage: `url('../assets/images/${project.programming_languages}.jpg')`,
+                                        backgroundRepeat: 'no-repeat',
+                                        backgroundSize: 'cover',
+                                        backgroundPosition: 'center',
+                                    }}
+                                />
                                 <Typography variant="h3" gutterBottom align="center">{project.project_name}</Typography>
                                 <Typography variant="h5" gutterBottom align="center" className="my-2">{project.programming_languages}</Typography>
                                 <Typography variant="h6" gutterBottom align="center" className="my-2">{project.description}</Typography>
                                 <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    href={project.github_link}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    GitHub Repository
-                                </Button>
-                            </div>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        href={project.github_link}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        GitHub Repository
+                                    </Button>
+                                </div>
                             </Paper>
                         </Grid>
                     ))}
