37b5841d0dadb9f6b6004d092271d7fdb3472dc1
Zach Coscarart <zachcoscarart@gmail.com>
2024-03-17 01:17:08 -0700

    LETS GO

--- src/components/Datatable.tsx
+++ src/components/Datatable.tsx
@@ -82,16 +82,16 @@ const DataTable = () => {
                                     checked={selectionModel.includes(project.id)}
                                     onChange={() => handleSelectionChange(project.id)}
                                 />
-                                {project.programming_languages && checkImageExists(`./src/assets/images/${project.programming_languages}.jpg`) ? (
+                                {project.programming_languages && checkImageExists(`https://raw.githubusercontent.com/ZCosco/Final_Portfolio/main/src/assets/images/${project.programming_languages}.jpg`) ? (
                                     <img
-                                        src={`./src/assets/images/${project.programming_languages}.jpg`}
+                                        src={`https://raw.githubusercontent.com/ZCosco/Final_Portfolio/main/src/assets/images/${project.programming_languages}.jpg`}
                                         className="mx-auto mb-3"
                                         style={{ maxWidth: '40%', height: 'auto' }}
                                         alt={project.programming_languages}
                                     />
                                 ) : (
                                     <img
-                                        src="./src/assets/images/default.jpg"
+                                        src="https://raw.githubusercontent.com/ZCosco/Final_Portfolio/main/src/assets/images/default.jpg"
                                         className="mx-auto mb-3"
                                         style={{ maxWidth: '40%', height: 'auto' }}
                                         alt="Default"
