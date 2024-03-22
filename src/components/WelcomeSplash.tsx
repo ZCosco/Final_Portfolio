abc57eca16280deb6550a064072c3c352caa16a1
Zach Coscarart <zachcoscarart@gmail.com>
2024-03-22 08:26:44 -0700

    title change

--- src/components/WelcomeSplash.tsx
+++ src/components/WelcomeSplash.tsx
@@ -7,19 +7,19 @@ const WelcomeSplash = () => {
         >
             {/* Welcome to My Portfolio */}
             <div className='flex place-items-center mb-20 bg-gray-900 text-white py-10 px-8 rounded-lg'>
-                <h3 className='p-5 text-white rounded text-5xl'>Welcome To My Portfolio!</h3>
+                <h3 className='p-5 text-white rounded text-5xl'>Zachariah Coscarart <br></br> Portfolio </h3>
             </div>
 
             {/* About Me Section */}
             <div className="bg-gray-900 text-white py-10 px-8 rounded-lg mb-10 max-w-3xl text-center"> {/* Limit the maximum width */}
-                <h2 className="text-4xl font-bold mb-4">About Me</h2>
+                <h2 className="text-4xl font-bold mb-4">Welcome</h2>
                 <p className="text-xl mb-4">
                     Hello all! My name is Zachariah Coscarart and I am currently a Coding Temple student taking the Software Development self-paces course. I currently live in Spokane, Washington!
                     I am new to the area but have always lived in Washington. Between Mt. Rainer, Seattle, and the various beaches, I would whole-heartedly reccomend giving our state a visit.
                     <br /><br />
                     This portfolio uses my Flask Portfolio API to access my database containing both my Coding Temple Projects table 
-                    and an empty Current Projects Table. The api has routes to [GET], [POST], [PUT], and [DELETE] my current projects, but for Coding Temple Projects I decided to onlt have the [GET] route. 
-                    This was because the Coding Temple projects are very much set and wouldn't need to be altered except for rare occasions in which I can use Dbeaver as the db 'Owner'. 
+                    and an empty Current Projects Table. The api has routes full CRUD routes for my current projects, but for Coding Temple Projects I decided to onlt have the [GET] route. 
+                    This was because the Coding Temple projects are very much set and wouldn't need to be altered except for rare occasions in which I can use Dbeaver as the db Owner. 
                     You will notice that In the Coding Temple Projects page it uses the CT Logo above each project while the image for Current Projects is varied. This is because for current projects it 
                     takes in the programming_language variable from the db and checks the images folder for a file with the same name. I have populated this folder with many different programming language 
                     logos to accomodate for various projects. Lastly I added a default image if none is initially found. 
